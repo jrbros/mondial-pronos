5d4d806371e5d736744f111af259ebecfd0b6efd
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2018-05-13 20:40:10 +0200

    refactoring Query Object and resolving sort issue

--- functions/graphql/schema/query.js
+++ functions/graphql/schema/query.js
@@ -4,7 +4,7 @@ const db = require("../../db").db;
 const matchesRef = db.ref("matches");
 const teamsRef = db.ref("teams");
 
-function joinMatchWithTeams(match) {
+function fetchMatchTeams(match) {
     if (match.is_known) {
         return Promise.all([
             teamsRef
@@ -24,34 +24,35 @@ function joinMatchWithTeams(match) {
     return match;
 }
 
+function fetchMatch(snapshot) {
+    const match = childSnapshot.val();
+    match.id = childSnapshot.key;
+    return fetchMatchTeams(match);
+}
+
+function fetchMatches(snapshot) {
+    const matches = [];
+    snapshot.forEach(childSnapshot => {
+        matches.push(fetchMatch(childSnapshot));
+    });
+    return Promise.all(matches);
+}
+
 const Query = {
     matches() {
         return matchesRef
             .orderByChild("date")
             .once("value")
-            .then(data => {
-                const matches = data.val();
-                return Promise.all(
-                    Object.keys(matches).map(id => {
-                        const match = matches[id];
-                        match.id = id;
-                        return joinMatchWithTeams(match);
-                    })
-                );
-            })
-            .catch(err => console.error("ERROR", err));
+            .then(fetchMatches)
+            .catch(console.error);
     },
 
     match(_, params) {
         return matchesRef
             .child(params.id)
             .once("value")
-            .then(data => {
-                const match = data.val();
-                match.id = params.id;
-                return joinMatchWithTeams(match);
-            })
-            .catch(err => console.error("ERROR", err));
+            .then(fetchMatch)
+            .catch(console.error);
     }
 };
 
